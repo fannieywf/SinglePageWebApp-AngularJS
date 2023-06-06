c749aecf4cf7eb69e2e7d7caf440fbe297a80e4a
Fannie Yuen <fannieywf@gmail.com>
2023-06-06 11:39:07 +0200

    fucntion -> function

--- L05/app.js
+++ L05/app.js
@@ -1,10 +1,17 @@
-(fucntion () {
-'use strict'
+(function () {
+'use strict';
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function() {
+// share data between view model and view: $scope
+// $ sign in front of some variable name: reserved for Angular
 
+.controller('MyFirstController', function ($scope) {
+// 1. $scope.xx
+    $scope.name = "Fannie";
+    $scope.sayHello = function () {
+        return "Hello!";
+    };
 });
 
 })();
